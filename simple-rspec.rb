e80a12b4c60fd856d87064663f7df936f30907bf
serge-tj <serge@tommyjohn.com>
2018-02-22 08:59:11 -0500

    A simple test file to get started with rspec

---
+++ simple-rspec.rb
@@ -0,0 +1,17 @@
+class My_math
+
+
+
+end    
+
+def add(x,y)
+    return  x + y
+ end
+ 
+ def subtract(x,y)
+     return y-x 
+ end    
+ 
+ def multiply(x,y)
+     return x * y 
+ end     
---
+++ simple-rspec_spec.rb
@@ -0,0 +1,10 @@
+require 'simple-rspec_rspec.rb'
+
+
+#examples 
+describe 'add' do 
+     it "adds numbers" do 
+    expect(add(4,5)).to eq(9)
+    end    
+end     
+  
