1c63120ff00fefef8f519f7b975d0eb038b6f9bf
serge-tj <serge@tommyjohn.com>
2018-02-23 14:07:47 -0500

    Tests to insure rspec is working

--- spec/simple-rspec_spec.rb
+++ spec/simple-rspec_spec.rb
@@ -1,10 +1,20 @@
-require 'simple-rspec_rspec.rb'
+require './lib/simple-rspec.rb'
 
 
 #examples 
 describe 'add' do 
      it "adds numbers" do 
     expect(add(4,5)).to eq(9)
+    end
+end    
+describe 'muliply' do
+    it "multiplys numbers" do 
+      expect(multiply(4,5)).to eq(20)
     end    
-end     
+end 
+describe 'subtract' do
+ it "subtract's numbers" do
+    expect(subtract(6,4)).to eq(2)   
+ end   
+end    
   
